fc97df62125eb0f6101403496d87324e8ad9b5f4
VashUber <mrjarokvk@gmail.com>
2022-12-05 17:12:57 +0300

    fix: jest config type

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
+import type { Config } from "jest";
+
 export default {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
+} as Config;
